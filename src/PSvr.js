dfcff3e6775ab5d79cd127a34cd2c645ac40bee2
Ruei-Jr Wu <rueijrwu@bu.edu>
2017-12-02 17:19:17 -0500

    Edit PSvr

--- src/PSvr.js
+++ src/PSvr.js
@@ -6,13 +6,19 @@ class PSvr extends Component {
     return (
       <div>
         <h3>PhaseSapce Virtual Reality</h3>
-        <img src={PUBLIC_URL + "/image/psvr.jpg"} alt="psvr" style={{"width":"960"}}/>
+        <img src={PUBLIC_URL + "/image/psvr.jpg"} alt="psvr" style={{"width":"960px"}}/>
         <p style={{"text-align":"justify"}}>
-          Psvr is a system which integrates PhaseSpace and Head Mount Display 
-          (HMD). PhaseSapce system use active infrared optical markers to capture 
-          precise body motion. PSvr is able to measure small head motion at high 
-          speed and present image to HMD to achieve precise Virtual Reality.
+          PhaseSpace System is an active optical system composed by cameras and 
+          64 LED markers. These cameras can measure distance of markers and compute
+          their position in space. To study natural, head-free, ocular motion, I 
+          innovate a helmet system based on PhaseSpace. Compare other devices using 
+          gyroscope or acceleration sensor, this system can measure head motion 
+          with high resolution at high speed.
         </p>
+        {/* <p>
+          PSvr is an integrated system with PhaseSapce helmet and Oculus Rift DK1. 
+          Instead of using the motion sensor from DK1,  
+        </p> */}
       </div>
     )
   }
