0a208bb3491d99923fe1d430211f44f4b0b63bd7
Ruei-Jr Wu <rueijrwu@bu.edu>
2017-12-02 16:35:01 -0500

    Edit PSvr

--- src/PSvr.js
+++ src/PSvr.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 
 class PSvr extends Component {
   render() {
+    var PUBLIC_URL = process.env.PUBLIC_URL;
     return (
       <div>
         <h3>PhaseSapce Virtual Reality</h3>
-        <img src="portfolio/image/psvr.jpg" alt="psvr" style={{"width":"800px"}}/>
+        <img src={PUBLIC_URL + "/image/psvr.jpg"} alt="psvr" style={{"width":"960"}}/>
         <p style={{"text-align":"justify"}}>
-        Psvr is a system which integrates PhaseSpace and Head Mount Display 
-        (HMD). PhaseSapce system use active infrared optical markers to capture 
-        precise body motion. PSvr is able to measure small head motion at high 
-        speed and present image to HMD to achieve precise Virtual Reality.
+          Psvr is a system which integrates PhaseSpace and Head Mount Display 
+          (HMD). PhaseSapce system use active infrared optical markers to capture 
+          precise body motion. PSvr is able to measure small head motion at high 
+          speed and present image to HMD to achieve precise Virtual Reality.
         </p>
       </div>
     )
