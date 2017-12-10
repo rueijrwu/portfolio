0a1e98aad30fe18a0b4d423e2396937958ee4b50
Ruei-Jr Wu <rueijrwu@bu.edu>
2017-12-10 15:46:08 -0500

    v2

--- src/APenv.js
+++ src/APenv.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 class APenv extends Component {
   render() {
+    var PUBLIC_URL = process.env.PUBLIC_URL;
     return (
       <div className="App-content">
         <img src={PUBLIC_URL + "/image/me.jpg"} alt="me" width="300" className="aboutme-img"/>
         <h3>APenv</h3>
         <p>
-          I developed APenv which integrates different devices includes DPI, 
-          Bionocular-DPI, DDPI, BOSS, Phasespace and HMD. APenv is a C++ library 
-          and designed for neuron, biomedical scientists and psychologist to allow 
-          them build visual interactive experiments on regular monitor and HMD very 
-          easily.      
+          I developed APenv, a 3D engine in C++. APenv integrates different devices 
+          includes DPI, Bionocular-DPI, DDPI, BOSS, Phasespace and HMD. APenv is 
+          designed for neuron, biomedical scientists and psychologist to allow them 
+          build interactive visual experiments on regular monitor and HMD very easily.      
         </p>
       </div>
     )
