4d0cb5a39aa1026b567a88a19c02fa4e33d7743f
rueijrwu <rueijrwu@bu.edu>
2017-12-10 15:47:37 -0500

    v2

---
+++ src/Termite.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Termite extends Component {
+  render() {
+    var PUBLIC_URL = process.env.PUBLIC_URL;
+    return (
+      <div>
+        <h3>Termite Robots</h3>
+        <img src={PUBLIC_URL + "/image/termite.png"} alt="termite" style={{"width":"960px"}}/>
+        <p style={{"text-align":"justify"}}>
+          TERMES is a decentralized distributed system to automatically build
+          3D structure by using precompiled single-path algorithm. However, most
+          structure is complicated and can not be constructed by a single path.
+          I developed new algorithm and software package to build 3D structure 
+          with multi path. This algoirithm defines the probability of each path 
+          and guides TERMES robots. 
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Termite
